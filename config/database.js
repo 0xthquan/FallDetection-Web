ea91cdabfe74536c9ba4a25b2bfd97b9b760aa85
darkwanderer2114 <tranquan2114@gmail.com>
2018-05-21 00:12:17 +0700

    change

--- config/database.js
+++ config/database.js
@@ -1,20 +1,20 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: '',
-    database: 'db_falldetection'
-});
-
 // var connection = mysql.createConnection({
-//     host: 'sql9.freemysqlhosting.net',
+//     host: 'localhost',
 //     port: 3306,
-//     user: 'sql9237825',
-//     password: 'XdwAFaSM3Z',
-//     database: 'sql9237825'
+//     user: 'root',
+//     password: '',
+//     database: 'db_falldetection'
 // });
+
+var connection = mysql.createConnection({
+    host: 'sql9.freemysqlhosting.net',
+    port: 3306,
+    user: 'sql9237825',
+    password: 'XdwAFaSM3Z',
+    database: 'sql9237825'
+});
 
 connection.connect();
 
